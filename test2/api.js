46db4f0fc542b1baa27c60fd3dc60cbc64d70244
jmrodev <juanmarcelo.rodrigueztandil@gmail.com>
2022-07-06 21:53:44 -0300

    intentado el edit

--- test2/api.js
+++ test2/api.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 let url = 'https://62b09dd5196a9e987027a824.mockapi.io/servicio'
 const formulario = document.querySelector('#formulario');
+const edit = document.querySelector('#edit');
 const actividad = document.querySelector('#actividad');
 const precio = document.querySelector('#precio');
 const idData = document.querySelector('#idData');
@@ -17,11 +18,11 @@ list.addEventListener('click', e => {
 }
 );
 
-// formulario.addEventListener('submit', e => {
-//     e.preventDefault();
-//     setData(e);
-// }
-// );
+formulario.addEventListener('submit', e => {
+    e.preventDefault();
+    setData(e);
+}
+);
 
 const getData = async () => {
     const response = await fetch(url);
@@ -29,72 +30,125 @@ const getData = async () => {
     view(dataObject);
 }
 
-
-
 const view = (dataObject) => {
     list.innerHTML = '';
 
     Object.values(dataObject).forEach(item => {
         const clone = template.cloneNode(true);
-
         clone.querySelector('td').textContent = item.id;
         clone.querySelectorAll('td')[0].textContent = item.actividades;
         clone.querySelectorAll('td')[1].textContent = item.precio;
         clone.querySelectorAll('.btn')[0].dataset.id = item.id;
         clone.querySelectorAll('.btn')[1].dataset.id = item.id;
-
         fragment.appendChild(clone);
-
     });
     list.appendChild(fragment);
 }
 
 const btnAction = e => {
-    console.log(e.target);
     if (e.target.classList.contains('btn-edit')) {
-        console.log(e.target.dataset.id);
-        getData();
+        toShow(e.target.dataset.id);
     }
     if (e.target.classList.contains('btn-delete')) {
-        console.log(e.target.dataset.id);
         deleteData(e.target.dataset.id);
     }
-
 }
 
 let deleteData = async (id) => {
     try {
-        const response = await fetch(`${url}/${id}`, {
+        const res = await fetch(`${url}/${id}`, {
             method: 'DELETE'
-        });
-        const dataObject = await response.json();
-        view(dataObject);
-        getData();
-    }
-    catch (error) {
+        }).then(response => response.json())
+            .then(a => {
+                getData();
+                console.log(a);
+            })
+    } catch (error) {
         console.log(error);
     }
 }
 
-
-
-
-
 const setData = (e) => {
-    if (input.value.trim() === '') {
-        console.log('No puedes dejar la tarea en blanco');
-        return;
+    e.preventDefault();
+    try {
+        const data = {
+            actividades: actividad.value,
+            precio: precio.value
+        }
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(a => {
+                console.log(a);
+                getData();
+            }
+            )
+    } catch (error) {
+        console.log(error);
     }
-    const data = {
-        id: Date.now(),
-        actividad: actividad.value,
-        precio: precio.value
-    }
-    data[data.id] = data;
-
-    console.log(e.data);
-    formulario.reset();
-    data.focus();
-
-    getData();
 }
+
+function toShow() {
+    
+        
+        edit.removeAttribute('hidden');
+        formulario.addAttribute('hidden');
+
+    } 
+    
+    // else {
+    //     const editData = (id) => {
+    //         fetch(`${url}/${id}`, {
+    //             method: 'PUT',
+    //             body: JSON.stringify({
+    //                 actividades: actividad.value,
+    //                 precio: precio.value
+    //             }),
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             }
+    //         })
+    //             .then(response => response.json())
+    //             .then(a => {
+
+    //                 console.log(a);
+    //                 getData();
+    //             }
+    //             )
+
+    //     }
+    // }
+
+
+// const editData = (id) => {
+//     console.log(id);
+//     e.preventDefault();
+//     try {
+//         const data = {
+//             actividades: actividad.value,
+//             precio: precio.value
+//         }
+//         fetch(`${url}/${id}`, {
+//             method: 'PUT',
+//             body: JSON.stringify(data),
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         })
+//             .then(response => response.json())
+//             .then(a => {
+//                 console.log(a);
+//                 getData();
+//             }
+//             )
+
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
